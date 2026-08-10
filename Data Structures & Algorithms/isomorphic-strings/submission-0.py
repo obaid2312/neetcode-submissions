009de112a098cd4dae71dd3d53f92596b0bfde5c
obaid <151417783+obaid2312@users.noreply.github.com>
2026-08-10 11:23:12 +0530

    Add: isomorphic-strings - submission-0

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        m1, m2 = [0] * 256, [0] * 256
+
+        n = len(s)
+
+        for i in range(n):
+            if m1[ord(s[i])] != m2[ord(t[i])]:
+                return False
+            
+            m1[ord(s[i])] = i + 1
+            m2[ord(t[i])] = i + 1
+        return True
